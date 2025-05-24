4dac6a3dff81dcd313ee7a4384c23c56ecfff575
Atul Jaiswal <ajais1001@gmail.com>
2025-05-24 19:55:33 +0530

    exploring commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
-app.use("/ ",(req,resp)=>{
-    resp.send("hello hello");
+// this will test the get method
+app.get("/user",(req,res)=>{
+  res.send('firstName=atul,lastName=sharma,age=25');  
 })
- app.use("/test",(req,resp)=>{
-    resp.send('Hello World');
- })
+// this will test the post method
+app.post("/user",(req,res)=>{
+  res.send('User data received');   
+});
+// this will test all htpp methods
+app.use('/test', (req, res) => {
+  res.send('Test route is working');
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');    
