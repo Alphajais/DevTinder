49cf47adede7289ebcba4e3a09cad1d155399e3f
Atul Jaiswal <ajais1001@gmail.com>
2025-05-31 02:16:30 +0530

    Api level Sanitation

--- src/app.js
+++ src/app.js
@@ -87,6 +87,20 @@ catch(err){
 }
 });
 
+app.get('/user/:id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    // Validate MongoDB ObjectId forma
+    const user = await User.findById(userId);
+res.send(user);
+  }
+  catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  } 
+  
+});
 
 
 
